214162df437b92e03747c1b6b7b73dbface862c2
lucianocjesus <lucianocjesus@gmail.com>
2016-07-15 10:52:40 -0300

    .

--- ProjetoTestePerson.Data/AssuntoData.cs
+++ ProjetoTestePerson.Data/AssuntoData.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ProjetoTestePerson.Domain;
 
 namespace ProjetoTestePerson.Data
